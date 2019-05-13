fbef971f640ee0f917d18b7868830928e9445f6b
quanghuy1242 <katygaga124@gmail.com>
2019-05-13 20:21:10 +0700

    added: index route, mongo interact

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const indexRoute = require('./routes/index.route')
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+mongoose.connect(process.env.MONGODB_URL, { useCreateIndex: true, useNewUrlParser: true });
+app.set('port', process.env.PORT || 3001);
+
+app.use('/', indexRoute);
 
 app.listen(app.get('port'), () => {
   console.log(`The server is running under port ${app.get('port')}`)
---
+++ models/project.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+let projectSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  date: { type: Date, required: true, default: Date.now },
+  image: { type: String, required: true },
+  url: { type: String, required: true },
+  screenshots: { type: [String] }
+});
+
+let Project = mongoose.model("Project", projectSchema);
+module.exports = Project;
