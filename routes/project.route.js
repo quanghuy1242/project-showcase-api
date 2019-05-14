53638969d40a80a4dcdd5c7ad5b6fa18f3d68dd6
quanghuy1242 <katygaga124@gmail.com>
2019-05-14 08:38:23 +0700

    added: get project by id route

--- routes/project.route.js
+++ routes/project.route.js
@@ -9,4 +9,14 @@ router.get('/', async (req, res) => {
   res.json({ projects: projects });
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const project = await Project.findById(id);
+    res.json({ project: project });
+  } catch (error) {
+    res.status(404).json({ message: 'Not Found' });
+  }
+});
+
 module.exports = router;
