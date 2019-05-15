0a70b167d66373027561ec2776548dcb2538cef8
quanghuy1242 <katygaga124@gmail.com>
2019-05-15 19:43:41 +0700

    updated: project route for support technical model

--- routes/project.route.js
+++ routes/project.route.js
@@ -2,11 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const Project = require('../models/project.model');
+const Technical = require('../models/technical.model'); // Cần để populate
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const projects = await Project.find().sort({ date: 'descending' });
+  const projects = await Project.find()
+    .sort({ date: 'descending' })
+    .populate('technical');
   res.json({ projects: projects });
 });
 
@@ -14,7 +17,7 @@ router.get('/:id', async (req, res, next) => {
   const { id } = req.params;
   if (!mongoose.Types.ObjectId.isValid(id)) { return next(); }
   try {
-    const project = await Project.findById(id);
+    const project = await Project.findById(id).populate('technical');
     res.json({ project: project });
   } catch (error) {
     res.status(404).json({ message: 'Not Found' });
@@ -23,7 +26,9 @@ router.get('/:id', async (req, res, next) => {
 
 router.get('/search', async (req, res) => {
   const { query } = req.query;
-  const projects = await Project.find({ name: new RegExp(query, 'i') }).sort({ date: 'descending' });
+  const projects = await Project.find({ name: new RegExp(query, 'i') })
+    .sort({ date: 'descending' })
+    .populate('technical');
   res.json({ projects: projects });
 })
 
