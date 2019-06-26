c812490a7b6f4c4e13e9ec14704746dd16e9c9e4
quanghuy1242 <katygaga124@gmail.com>
2019-06-26 21:51:54 +0700

    fixed: undefined token

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -2,12 +2,14 @@ const verifyJwtToken = require('../utils/verifyJwtToken');
 require('dotenv').config();
 
 module.exports.privateRoute = async (req, res, next) => {
-  const token = req.headers['authorization'] || req.headers['x-access-token'];
-  if (token.startsWith('Bearer ')) { token = token.slice(7); }
+  let token = req.headers['authorization'] || req.headers['x-access-token'];
   
   if (!token) {
     return res.status(403).json({ msg: 'No token provided.' })
   }
+
+  if (token.startsWith('Bearer ')) { token = token.slice(7); }
+
   try {
     const decoded = await verifyJwtToken(token, process.env.JWT_SECRET);
     req.decoded = decoded;
