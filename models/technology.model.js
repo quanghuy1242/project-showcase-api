62855d0efae9bf7936ec23f5f4eca9b5b656a34f
quanghuy1242 <quanghuy1242@gmail.com>
2019-07-08 18:03:19 +0700

    changed: add validate fucntion

--- models/technology.model.js
+++ models/technology.model.js
@@ -7,5 +7,18 @@ let technologySchema = mongoose.Schema({
   image: { type: String, required: true }
 });
 
+technologySchema.statics.isValid = function({
+  _id, nameId, name, description, image
+}) {
+  if (!mongoose.Types.ObjectId.isValid(_id) && _id !== undefined) {
+    return false;
+  }
+  if (!/[a-zA-Z]+/.test(nameId)) { return false; }
+  if (!name.length !== 0) { return false; }
+  if (!description.length !== 0) { return false; }
+  if (!/(http|https):\/\/.+/.test(image)) { return false; }
+  return true;
+}
+
 const Technology = mongoose.model("Technology", technologySchema);
 module.exports = Technology;
