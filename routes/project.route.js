a8ba46575271b58d54e136c883483cbd7901b29a
quanghuy1242 <quanghuy1242@gmail.com>
2019-07-08 17:47:43 +0700

    changed: private router

--- routes/project.route.js
+++ routes/project.route.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const Project = require('../models/project.model');
 const Technology = require('../models/technology.model'); // Cần để populate
 
+const auth = require('../middlewares/auth.middleware');
+
 const router = express.Router();
 
 router.get('/', async (req, res) => {
@@ -37,7 +39,7 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
-router.post('/:id', (req, res) => {
+router.post('/:id', auth.privateRoute, (req, res) => {
   const project = req.body.project;
   if (!Project.isValid({ _id: undefined, ...project })) {
     return res.status(400).json({ msg: 'Data is not valid' });
@@ -49,7 +51,7 @@ router.post('/:id', (req, res) => {
   return res.json({ msg: 'New Project is added to database' });
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', auth.privateRoute, async (req, res) => {
   const project = req.body.project;
   if (!Project.isValid({ ...project })) {
     return res.status(400).json({ msg: 'Data is not valid' });
@@ -63,7 +65,7 @@ router.put('/:id', async (req, res) => {
   return res.json({ msg: `The project with id ${project._id} is updated` })
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth.privateRoute, async (req, res) => {
   const { _id } = req.body;
   if (!mongoose.Types.ObjectId.isValid(_id)) {
     return res.status(400).json({ msg: 'Data is not valid' });
