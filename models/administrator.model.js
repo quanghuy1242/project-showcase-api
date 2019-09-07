fc2f3d0f3f06cd231430c6444f0b67865f1b7661
quanghuy1242 <quanghuy1242@gmail.com>
2019-09-07 21:00:34 +0700

    added new property

--- models/administrator.model.js
+++ models/administrator.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 let administratorSchema = mongoose.Schema({
+  username: { type: String, required: true, unique: true },
   displayName: { type: String, required: true },
   name: { type: String, required: true },
   image: { type: String, required: true },
