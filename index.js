830cb49fce50f3a9a2360aa09de1201883b09c5a
Quang Huy <quanghuy1242@gmail.com>
2019-12-04 22:22:54 +0700

    update cors

--- index.js
+++ index.js
@@ -25,10 +25,20 @@ app.set('port', process.env.PORT || 3001);
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.SECRET_COOKIE));
 
-app.use(cors({
-  origin: (origin, callback) => callback(null, true),
-  optionsSuccessStatus: 200,
-  credentials: true
+app.use(cors((req, callback) => {
+  const whitelist = process.env.ALLOW_ORIGIN.split(';');
+  const clientAPIKey = req.header('CLIENT-KEY');
+  callback(null, {
+    origin: (origin, callback) => {
+      if (whitelist.includes(origin) || clientAPIKey === process.env.CLIENT_KEY) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    optionsSuccessStatus: 200,
+    credentials: true
+  });
 }));
 
 app.use('/', indexRoute);
