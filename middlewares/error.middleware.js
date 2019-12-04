876afad0544f040bc5d972c3ef71178bc1a255c3
Quang Huy <quanghuy1242@gmail.com>
2019-12-04 22:28:51 +0700

    updated error handling

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,3 +1,7 @@
-module.exports.errorHandling = (req, res) => {
-  res.status(404).json({ message: 'Not Found' })
+module.exports.errorHandling = (err, req, res, next) => {
+  if (err.message === 'Not allowed by CORS') {
+    res.status(500).json({ message: err.message })
+  } else {
+    res.status(404).json({ message: 'Not Found' })
+  }
 }
